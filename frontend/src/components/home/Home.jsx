e44c487eab00fc4eef80331928003cca55b905f1
Arthur <arthurclementte@hotmail.com>
2020-09-28 14:09:34 -0300

    Criado o componente que renderiza o conteúdo da página inicial.

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de controle de estoque.</p>
+    </Main>
